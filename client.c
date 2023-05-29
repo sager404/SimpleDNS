b58fed2b717d80d5269f1965b052c685171f8d17
sager <kingsager404@gmail.com>
2023-05-29 22:44:09 +0800

    implement client and localserver

--- client.c
+++ client.c
@@ -1,48 +1,152 @@
 #include "dns.h"
-#include <netinet/in.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <string.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-void gen_dns_header(struct DNS_Header *header);
-void gen_dns_query(struct DNS_Query *query,char* qname,short qtype);
+void gen_query_packet(char *packet, struct DNS_Header *header,
+                      struct DNS_Query *query, char *qname);
+void parse_dns_response(unsigned char *packet, struct DNS_RR *rr);
 
-int main(){
-    int sock;
-    struct sockaddr_in localserver_addr;
-    int s = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (s<0)
-        printf("socket failed");
+struct sockaddr_in localserver_addr;
+struct sockaddr_in client_addr;
+int sock;
+
+void init_client() {
+
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0) {
+        perror("client: socket failed");
+        close(sock);
+    }
+
     memset(&localserver_addr, 0, sizeof(localserver_addr));
     localserver_addr.sin_family = AF_INET;
-    localserver_addr.sin_addr.s_addr = inet_addr("127.0.0.2");
-    localserver_addr.sin_port = htons(UDP_PORT);
-
-    struct DNS_Header *header=(struct DNS_Header *)malloc(sizeof(struct DNS_Header));
-    struct DNS_Query *query=(struct DNS_Query *)malloc(sizeof(struct DNS_Query));
-    gen_dns_header(header);
-    char *qname="www.baidu.com";
-    gen_dns_query(query, qname, 1);
-    // memset(&header, 0, sizeof(header));
-    // header.id=htons(1);
-
-    // sendto(s, const void *buf, size_t n, 0, (struct sockaddr *) &localserver_addr, sizeof(localserver_addr));
-
+    localserver_addr.sin_addr.s_addr = inet_addr(LOCAL_SERVER_IP);
+    localserver_addr.sin_port = htons(DNS_PORT);
 }
 
-void gen_dns_header(struct DNS_Header *header){
+int main() {
+    init_client();
+    // if (connect(sock, (struct sockaddr *)&localserver_addr,
+    //             sizeof(localserver_addr)) < 1) {
+    //     perror("connect failed");
+    //     close(sock);
+    // }
+    char qname[127] = {0};
+    char qtype[127] = {0};
 
-    header->id=htons(1);
-    header->flags=0;
-    header->queryNum=htons(1);
-    header->answerNum=htons(0);
-    header->authorNum=htons(0);
-    header->addNum=htons(0);
+    char packetOut[BUFSIZE] = {0};
+    char packetIn[BUFSIZE] = {0};
+    // memset(packetOut, 0, sizeof(packetOut));
+    // memset(packetIn, 0, sizeof(packetIn));
 
+    printf("Input the domain:\n");
+    scanf("%s", qname);
+
+    unsigned short type = A;
+    // if (!strcmp(qtype, "A")) {
+    //     type = A;
+    // }else if (!strcmp(qtype, "MX")) {
+    //     type = MX;
+    // }else if (!strcmp(qtype, "NS")) {
+    //     type = NS;
+    // }else{
+    //     printf("Invalid type!");
+    // }
+
+    struct DNS_Header *header = malloc(sizeof(struct DNS_Header));
+    struct DNS_Query *query = malloc(sizeof(struct DNS_Query));
+    // memset(query, 0, strlen(qname)+5);
+    gen_dns_header(header, FLAGS_QUERY, 1, 0);
+    gen_dns_query(query, qname, type);
+    gen_query_packet(packetOut, header, query, qname);
+
+    if (sendto(sock, packetOut, BUFSIZE, 0,
+               (struct sockaddr *)&localserver_addr,
+               sizeof(localserver_addr)) < 0) {
+        perror("client: sendto failed");
+    }
+    unsigned int sock_len = 0;
+
+    if (recvfrom(sock, packetIn, BUFSIZE, 0, (struct sockaddr *)&client_addr,
+                 &sock_len) < 0) {
+        perror("client: Receive from server failed");
+    } else {
+        struct DNS_RR *rr = malloc(sizeof(struct DNS_RR));
+        parse_dns_response(packetIn, rr);
+        printf("%s", rr->rdata);
+        free(rr->rdata);
+        free(rr);
+    }
+
+    free(query->name);
+    free(header);
+    free(query);
 }
 
-void gen_dns_query(struct DNS_Query *query,char* qname,short qtype){
+void gen_query_packet(char *packet, struct DNS_Header *header,
+                      struct DNS_Query *query, char *qname) {
+    memcpy(packet, header, sizeof(struct DNS_Header));
+    int name_len = strlen(qname) + 2;
+    int offset = sizeof(struct DNS_Header);
+    memcpy(packet + offset, query->name, name_len);
+    offset += name_len;
+    memcpy(packet + offset, &query->qtype, sizeof(query->qtype));
+    offset += sizeof(query->qtype);
+    memcpy(packet + offset, &query->qclass, sizeof(query->qclass));
+}
 
+void parse_dns_response(unsigned char *packet, struct DNS_RR *rr) {
+    int i = 0, j = 0;
+    int name_len = 0;
+    int offset = 0;
+
+    i += sizeof(struct DNS_Header);
+    struct DNS_Header *header = (struct DNS_Header *)packet;
+    short queryNum = ntohs(header->queryNum);
+    short answerNum = ntohs(header->answerNum);
+    for (int n = 0; n < queryNum; n++) {
+        while (1) {
+            if (packet[i] == '\0') {
+                i++;
+                break;
+            }
+            i += packet[i] + 1;
+        }
+
+        i += 4;
+    }
+
+    for (int n = 0; n < answerNum; n++) {
+        if (packet[i] == 0xc0) {
+            offset = packet[i + 1];
+            i += 2;
+        }
+        // type
+        memcpy(&rr->type, packet + i, sizeof(rr->type));
+        i += sizeof(rr->type);
+        // class
+        memcpy(&rr->rclass, packet + i, sizeof(rr->rclass));
+        i += sizeof(rr->rclass);
+        // ttl
+        memcpy(&rr->ttl, packet + i, sizeof(rr->ttl));
+        i += sizeof(rr->ttl);
+        // length
+        memcpy(&rr->length, packet + i, sizeof(rr->length));
+        i += sizeof(rr->length);
+        if (ntohs(rr->type) == A) {
+            struct in_addr *addr;
+            memset(addr, 0, sizeof(struct in_addr));
+            addr->s_addr = *(unsigned int *)(packet + i);
+            // memcpy(&addr.sin_addr.s_addr,packet+i,4);
+
+            rr->rdata = malloc(16);
+            rr->rdata = inet_ntoa(*addr);
+            free(addr);
+        }
+    }
 }
--- dns.h
+++ dns.h
@@ -1,14 +1,29 @@
 #ifndef DNS_H_
 #define DNS_H_
 
-#define UDP_PORT 53
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUFSIZE 1024
+#define DNS_PORT 53000
 #define A 1
 #define NS 2
 #define CNAME 5
 #define MX 15
-#define PTR 12 
+#define PTR 12
+#define IN 1
+#define FLAGS_QUERY 0x0000
+#define FLAGS_RESPONSE 0x8000
+#define CLIENT_IP "127.0.0.1"
+#define LOCAL_SERVER_IP "127.0.1.1"
+#define ROOT_SERVER_IP "127.1.0.1"
 
-struct DNS_Header{
+struct DNS_Header {
     unsigned short id;
     unsigned short flags;
     unsigned short queryNum;
@@ -17,14 +32,14 @@ struct DNS_Header{
     unsigned short addNum;
 };
 
-struct DNS_Query{
+struct DNS_Query {
     unsigned char *name;
     unsigned short qtype;
     unsigned short qclass;
 };
 
-struct DNS_Response{
-    unsigned char *name;
+struct DNS_RR {
+    unsigned char name[2];
     unsigned short type;
     unsigned short rclass;
     unsigned int ttl;
@@ -32,5 +47,160 @@ struct DNS_Response{
     unsigned char *rdata;
 };
 
+extern void init_client();
+extern void init_localserver();
+
+static void serialize_addr(char *addr, char **rdata) {
+
+    in_addr_t in_addr = inet_addr(addr);
+    // unsigned char *ptr = &in_addr;
+    // for (int i = 0; i < 4; i++)
+    //     rdata[i] = ptr[i];
+    *rdata = (unsigned int *)&in_addr;
+}
+
+static void gen_dns_header(struct DNS_Header *header, short flags,
+                           short qdcount, short ancount) {
+
+    header->id = htons(1);
+    header->flags = htons(flags);
+    header->queryNum = htons(qdcount);
+    header->answerNum = htons(ancount);
+    header->authorNum = 0;
+    header->addNum = 0;
+}
+
+static void gen_dns_query(struct DNS_Query *query, char *name, short qtype) {
+    int len = strlen(name) + 1;
+    query->name = malloc(len + 1);
+    memcpy(query->name + 1, name, len);
+
+    int i = 0;
+    int m = 0;
+    char count = 0;
+    while (1) {
+        if (name[i] == '\0') {
+            query->name[m] = count;
+            break;
+        }
+        if (name[i] == '.') {
+            query->name[m] = count;
+            m += (count + 1);
+            count = 0;
+            i++;
+        } else {
+            i++;
+            count++;
+        }
+    }
+    query->qtype = htons(qtype);
+    query->qclass = htons(IN);
+}
+
+static void gen_dns_rr(struct DNS_RR *rr, short type, int ttl, char *addr,
+                       char offset) {
+    rr->name[0] = 0xc0;
+    rr->name[1] = offset;
+    rr->rclass = htons(IN);
+    rr->type = htons(type);
+    rr->ttl = htonl(ttl);
+    unsigned short len = 0;
+
+    if (type == A) {
+        len = 4;
+        rr->rdata = malloc(len);
+        serialize_addr(addr, &rr->rdata);
+    } else {
+        len = strlen(addr) + 1;
+        rr->rdata = malloc(len);
+        strcpy(rr->rdata, addr);
+    }
+
+    rr->length = htons(len);
+}
+
+static int parse_query_packet(char *packet, struct DNS_Header *header,
+                              struct DNS_Query *query, char *name) {
+    int i = 0;
+    int j = 0;
+    int name_len = 0;
+    int offset = 0;
+    offset = sizeof(struct DNS_Header);
+    i += offset;
+
+    for (int n = 0; n < ntohs(header->queryNum); n++) {
+        while (1) {
+            if (packet[i] == '\0') {
+                j--;
+                name[j] = '\0';
+                name_len = j;
+                query->name = malloc(name_len);
+                memcpy(query->name, name, name_len);
+                break;
+            }
+            memcpy(name + j, packet + i + 1, packet[i]);
+
+            j += packet[i];
+            i += packet[i] + 1;
+            name[j] = '.';
+            j++;
+        }
+    }
+
+    memcpy(&query->qtype, packet + i, sizeof(query->qtype));
+    i += sizeof(header->addNum);
+    memcpy(&query->qclass, packet + i, sizeof(query->qclass));
+    i += sizeof(query->qclass);
+
+    return i;
+}
+
+static void gen_response_packet(char *packet, struct DNS_Header *header,
+                                short answerNum) {
+    header->flags = htons(FLAGS_RESPONSE);
+    header->answerNum = htons(answerNum);
+    // memcpy(packet,header,sizeof(struct DNS_Header));
+}
+
+static void gen_dns_response(struct DNS_RR *answer, char *addr, char offset,
+                             short type, int ttl) {
+    answer->name[0] = 0xc0;
+    answer->name[1] = offset;
+    answer->type = htons(type);
+    answer->rclass = htons(IN);
+    answer->ttl = htonl(ttl);
+    answer->rdata = addr;
+}
+
+static short get_type(char *type) {
+    if (!strcmp("A", type))
+        return A;
+    else if (!strcmp("NS", type))
+        return NS;
+    else if (!strcmp("CNAME", type))
+        return CNAME;
+    else if (!strcmp("MX", type))
+        return MX;
+    else if (!strcmp("PTR", type))
+        return PTR;
+    else
+        return 0;
+}
+
+static void add_rr(char *packet, struct DNS_RR *rr, int offset) {
+    memcpy(packet + offset, rr->name, sizeof(rr->name));
+    offset += sizeof(rr->name);
+    memcpy(packet + offset, &rr->type, sizeof(rr->type));
+    offset += sizeof(rr->type);
+    memcpy(packet + offset, &rr->rclass, sizeof(rr->rclass));
+    offset += sizeof(rr->name);
+    memcpy(packet + offset, &rr->ttl, sizeof(rr->ttl));
+    offset += sizeof(rr->ttl);
+    memcpy(packet + offset, &rr->length, sizeof(rr->length));
+    offset += sizeof(rr->name);
+    int length = htons(rr->length);
+    memcpy(packet + offset, rr->rdata, length);
+    offset += length;
+}
 
 #endif
