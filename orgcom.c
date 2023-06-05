dc2429bd9e654886213335d6c6281d87f19d7107
Shuocheng Guo <2451431935@qq.com>
2023-06-05 19:36:10 +0800

    update orgcom4

--- orgcom.c
+++ orgcom.c
@@ -30,8 +30,7 @@ int main(){
 	dns_header *recvHead = (dns_header *)malloc(sizeof(dns_header));initHead(recvHead);
 	dns_rr *recvrRecord = (dns_rr *)malloc(sizeof(dns_rr));initRR(recvrRecord);  
 	//回应的结构体 
-	dns_query *resQuery = (dns_query *)malloc(sizeof(dns_query));initQuery(resQuery);
-	dns_header *resHead = (dns_header *)malloc(sizeof(dns_header));initHead(resHead);
+
 	dns_rr *resRecord = (dns_rr *)malloc(sizeof(dns_rr));initRR(resRecord);
 	//MX第二次查询ip
 	dns_query *mxQuery = (dns_query *)malloc(sizeof(dns_query));initQuery(mxQuery);
@@ -45,32 +44,23 @@ int main(){
     int query_len = deserialize_query(i + 2 + header_len, recvQuery); 	
 	printf("The domain name is: %s\n", recvQuery->name);
 	
-	//以下为回应的部分
-	resHead->id =htons(recvHead->id);
-	resHead->flags =htons(0x8000);
-	resHead->queryNum =htons(recvHead->queryNum);
-	resHead->answerNum = htons(1); 
-	resHead->authorNum = 0;
-	resHead->addNum = 0;
-	resQuery = recvQuery;
-	resRecord->name=recvQuery->name;
-    resRecord->rclass=recvQuery->qclass;
-	resRecord->type=recvQuery->qtype;
-	resRecord->ttl = (uint32_t)86400;
-	resRecord->length = 4;
 	
 	/*
 	 *返回查询结果 
 	 */
 	memcpy(packetOut,packetIn,BUFSIZE);
-	if(get_ORGCOM(packetOut, recvQuery, 14+query_len)){
+	dns_query *resQuery = (dns_query *)malloc(sizeof(dns_query));
+	dns_header *resHead = (struct DNS_Header *)(packetOut+2);
+	int len = parse_query_packet(packetOut+2,resHead,resQuery);
+	if(get_ORGCOM(packetOut+2, resQuery, len)){
 		unsigned int len_p = htons(cal_packet_len(packetOut+2));
-		struct DNS_Header *header = (struct DNS_Header *)(packetOut+2);
-		header->flags=htons(FLAGS_RESPONSE);
+		resHead->flags=htons(FLAGS_RESPONSE);
 		memcpy(packetOut, &len_p, 2);
 		tcp_send(client_sock, packetOut, len_p+2);
 	}
 	}   
+	close(sock);
+	
 }	
 
 
