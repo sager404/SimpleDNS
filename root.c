986c83a0253d74fb4940521d71fea13b3861cbd2
SakiRinn <sakirinn@qq.com>
2023-06-05 18:37:15 +0800

    root done.

--- root.c
+++ root.c
@@ -37,32 +37,34 @@ int main() {
         while (rlen = tcp_receive(client_sock, buffer)) {
             header_len = deserialize_header(buffer + 2, header);
             query_len = deserialize_query(buffer + 2 + header_len, query);
-        }
-        memset(buffer, 0, BUFSIZE);
 
-        struct DNS_RR *RRs;
-        int cnt = get_root_data(RRs);
-        int offset = 2 + header_len + query_len;
-        header->answerNum = 0;
-        header->authorNum = htons(1);
-        header->addNum = htons(1);
+            memset(buffer, 0, BUFSIZE);
+            int length = 2 + header_len + query_len;
+            struct DNS_RR *RRs;
+            int cnt = get_root_data(RRs);
 
-        int ns_idx = find_ns(RRs, cnt, query);
-        if (ns_idx != -1) {
-            int a_idx = find_a_corresponding_ns(RRs, cnt, RRs[ns_idx].rdata);
-            if (a_idx == -1) {
-                perror("Database error!");
-                exit(EXIT_FAILURE);
+            header->answerNum = 0;
+            header->authorNum = htons(1);
+            header->addNum = htons(1);
+
+            int ns_idx = find_ns(RRs, cnt, query);
+            if (ns_idx != -1) {
+                int a_idx =
+                    find_a_corresponding_ns(RRs, cnt, RRs[ns_idx].rdata);
+                if (a_idx == -1) {
+                    perror("Database error!");
+                    exit(EXIT_FAILURE);
+                }
+                header->flags = htons(gen_flags(1, OP_STD, 1, R_FINE));
+                gen_response(buffer, header, query);
+                length += add_rr(buffer + length, RRs + ns_idx);
+                length += add_rr(buffer + length, RRs + a_idx);
+            } else {
+                header->flags = htons(gen_flags(1, OP_STD, 1, R_NAME_ERROR));
+                gen_response(buffer, header, query);
             }
-            header->flags = htons(gen_flags(1, OP_STD, 1, R_FINE));
-            gen_response(buffer, header, query);
-            offset += add_rr(buffer + offset, RRs + ns_idx);
-            offset += add_rr(buffer + offset, RRs + a_idx);
-        } else {
-            header->flags = htons(gen_flags(1, OP_STD, 1, R_NAME_ERROR));
-            gen_response(buffer, header, query);
+            tcp_send(client_sock, buffer, length);
         }
-
         close(client_sock);
     }
     close(sock);
